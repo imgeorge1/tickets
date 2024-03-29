00a8b3e62d92254758e5a2788df1587799002de1
User <Ubitoz133@gmail.com>
2024-03-29 21:14:19 +0400

    added links

---
+++ assets/sign-icons/short-links.js
@@ -0,0 +1,7 @@
+// 1: https://tinyurl.com/5fvx87ck
+// 2: https://tinyurl.com/mrybry2h
+// 3: https://tinyurl.com/yyb5yfrh
+// 4: https://tinyurl.com/yru7fm4c
+// 5: https://tinyurl.com/498r9nft
+// 6: https://tinyurl.com/54pz245u
+// 7: https://tinyurl.com/39fzak3u
